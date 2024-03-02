df02f032bc31544f0e8666c7c4578548ac534e32
Saahil Patel <WorkSP404@outlook.com>
2024-03-01 23:16:37 -0500

    Final Version

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -4,16 +4,19 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace ClassInventory
 {
     public partial class Form1 : Form
     {
         // Global Variabales go here
-
+        List<playerObjects> playerObjectsList = new List<playerObjects>();
+        
 
         public Form1()
         {
@@ -22,12 +25,82 @@ namespace ClassInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            string name = nameInput.Text;
+            string team = teamInput.Text;
+            string position = positionInput.Text;
+            int age = Convert.ToInt32(ageInput.Text);
 
+            playerObjects player = new playerObjects(name, team, position, age);
+            playerObjectsList.Add(player);
+
+            label1.Text = "Player Created";
+        }
+        private void ListDisplaySort()
+        {
+            playerObjectsList.Sort();
         }
 
+        private void PlayerSearch(TextBox textBox, string command)
+        {
+            //Lambda code
+            string name = textBox.Text;
+            playerObjects playerFound = playerObjectsList.Find(t => t.name == name);
+
+            if (command == "remove")
+            {
+                playerObjectsList.Remove(playerFound);
+                label1.Text = "player was removed";
+            }
+            else
+            {
+                label1.Text = "no player found";
+            }
+
+            if (command == "display")
+            {
+                if (playerFound == null)
+                {
+                    label1.Text = "No player found";
+                }
+                else
+                {
+                    DisplayPlayers();
+                }
+            }
+            if (command == "search")
+            {
+                label1.Text = playerFound.name;
+                label1.Text = $"{playerFound.name} {playerFound.team} {playerFound.position} {playerFound.age}\n";
+            }
+        }
         private void removeButton_Click(object sender, EventArgs e)
         {
 
+            PlayerSearch(removeInput, "remove");
+
+            ////Normal Solution
+            //int listSize = playerObjectsList.Count();
+            //foreach (playerObjects p in playerObjectsList)
+            //{
+            //    if (p.name == name)
+            //    {
+            //        playerObjectsList.Remove(p);
+            //        break; //break out of the foreach loop
+            //    }
+            //}
+            //if(listSize == playerObjectsList.Count)
+            //{
+            //    label1.Text = "Nothing was found";
+            //}
+            //DisplayPlayers();
+        }
+        private void DisplayPlayers()
+        {
+            label1.Text = "";
+            for (int i = 0; i < playerObjectsList.Count; i++)
+            {
+                label1.Text += $"{playerObjectsList[i].name} {playerObjectsList[i].team} {playerObjectsList[i].position} {playerObjectsList[i].age}\n";
+            }
         }
 
         private void searchButton_Click(object sender, EventArgs e)
@@ -35,12 +108,13 @@ namespace ClassInventory
             // This is to be completed in Part II. You will use 
             // Lambda Expressions. 
             //---------------------------
-
+            PlayerSearch(nameSearchInput,"search");
+            
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
-
+            DisplayPlayers();
         }
     }
 }
---
+++ ClassInventory/playerObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ClassInventory
+{
+    internal class playerObjects
+    {
+        public string name, team, position;
+        public int age;
+        public playerObjects(string _name, string _team, string _position, int _age)
+        {
+            name = _name;
+            team = _team;
+            position = _position;
+            age = _age;
+
+        }
+    }
+}
